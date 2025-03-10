e1cbb818afc390b015fdd978ea0aad6fb5b82680
Shakirov Marsel <imanager666@gmail.com>
2025-03-10 15:54:10 +0300

    delete all console

--- src/shared/hooks/useEnterPressButton.js
+++ src/shared/hooks/useEnterPressButton.js
@@ -4,7 +4,7 @@ export const useEnterPressButton = (callback, isDisabled = false) => {
 	useEffect(() => {
 		function handleEnterKeyUp(event) {
 			const isNotButton = event.target.tagName !== 'BUTTON';
-			console.log(event.target.tagName, isNotButton);
+
 			if (!isDisabled && isNotButton && event.code === 'Enter') {
 				callback();
 			}
